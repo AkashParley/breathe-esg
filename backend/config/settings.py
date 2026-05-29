daf8571b8856a0abddc45501b9592a47d049f6fc
AkashParley <AkashParley@github.com>
2026-05-29 13:56:53 +0530

    fix: allow all CORS origins for production

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -85,6 +85,10 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
 ]
 
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
